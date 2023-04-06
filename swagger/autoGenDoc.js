6e10e4f69db8bbf785b7120f4998caaeb1edb879
guizao <lopesgui973@gmail.com>
2023-04-06 13:32:43 -0300

    Mudando dominio do servidor de produção

--- swagger/autoGenDoc.js
+++ swagger/autoGenDoc.js
@@ -19,7 +19,7 @@ let doc = {
       description: "Servidor localhost.",
     },
     {
-      url: "https://dnc-board-tasks-back.vercel.app/",
+      url: "https://bt-back.vercel.app/",
       description: "Servidor de produção.",
     },
   ],
