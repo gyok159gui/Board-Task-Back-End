2edee5832bd2fd1bcaaf05e2a9573fd3e07284ad
guizao <lopesgui973@gmail.com>
2023-04-07 08:54:56 -0300

    Nova rota login Usuario

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,6 +2,7 @@ const express = require("express");
 const conectarBancoDados = require("../middlewares/conectarBD");
 const tratarErrosEsperados = require("../functions/tratarErrosEsperados");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const EsquemaUsuario = require("../models/usuario");
 const router = express.Router();
 
@@ -29,6 +30,35 @@ router.post("/criar", conectarBancoDados, async function (req, res) {
       );
     }
     return tratarErrosEsperados(res, error);
+  }
+});
+
+router.post("/logar", conectarBancoDados, async function (req, res) {
+  try {
+    // #swagger.tags = ['Usuario']
+    let { email, senha } = req.body;
+
+    let respostaBD = await EsquemaUsuario.findOne({ email }).select("+senha");
+    if (respostaBD) {
+      let senhaCorreta = await bcrypt.compare(senha, respostaBD.senha);
+      if (senhaCorreta) {
+        let token = jwt.sign({ id: respostaBD._id }, process.env.JWT_SECRET, {
+          expiresIn: "1d",
+        });
+        res.header("x-auth-token", token);
+        res.status(200).json({
+          status: "Ok",
+          statusMensagem: "Usuário autenticado com sucesso",
+          resposta: { "x-auth-token": token },
+        });
+      } else {
+        throw new Error("Email ou senha incorreta");
+      }
+    } else {
+      throw new Error("Email ou senha incorreta");
+    }
+  } catch (err) {
+    return tratarErrosEsperados(res, err);
   }
 });
 
