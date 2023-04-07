4fc99f5d7441f291366b4e936b60f10b8bc6ed6e
guizao <lopesgui973@gmail.com>
2023-04-07 14:53:34 -0300

    Teste de fix erro

--- src/middlewares/conectarBD.js
+++ src/middlewares/conectarBD.js
@@ -1,16 +1,11 @@
 const mongoose = require("mongoose");
-const tratarErrosEsperados = require("../functions/tratarErrosEsperados.js");
+const tratarErrosEsperados = require("../functions/tratarErrosEsperados");
 
 async function conectarBancoDados(req = null, res = null, next = null) {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Conectado ao banco de dados!");
-    try {
-      next();
-    } catch {}
+    try { next(); }    catch { };
     return mongoose;
   } catch (error) {
     console.error(error);
