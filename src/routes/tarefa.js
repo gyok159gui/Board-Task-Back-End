b5191100d9cb6f7ec423037324a84a4473ad5967
guizao <lopesgui973@gmail.com>
2023-04-08 10:17:50 -0300

    Criação de function para deletar tarefa do usuario logado

--- src/routes/tarefa.js
+++ src/routes/tarefa.js
@@ -74,15 +74,46 @@ router.get(
     try {
       // #swagger.tags = ['Tarefa']
       // #swagger.description = "Endpoint para obter todas as tarefas do usuario logado"
-      let { posicao, titulo, descricao, status, dataEntrega } = req.body;
       const usuarioLogado = req.usuarioJwt.id;
       const respostaBD = await EsquemaTarefa.find({
         usuarioCriador: usuarioLogado,
-      }).populate(usuarioCriador);
+      }).populate("usuarioCriador");
 
       res.status(200).json({
         status: "OK",
         statusMensagem: "Tarefas listadas na resposta com sucesso.",
+        resposta: respostaBD,
+      });
+    } catch (error) {
+      return tratarErrosEsperados(res, error);
+    }
+  }
+);
+
+router.delete(
+  "/delete/:id",
+  authUser,
+  conectarBancoDados,
+  async function (req, res) {
+    try {
+      // #swagger.tags = ['Tarefa']
+      const idTarefa = req.params.id;
+      const usuarioLogado = req.usuarioJwt.id;
+
+      const checkTarefa = await EsquemaTarefa.findOne({
+        _id: idTarefa,
+        usuarioCriador: usuarioLogado,
+      });
+      if (!checkTarefa) {
+        throw new Error("Tarefa não encontrada ou pertence a outro usuário");
+      }
+
+      const respostaBD = await EsquemaTarefa.deleteOne({
+        _id: idTarefa,
+      });
+      res.status(200).json({
+        status: "OK",
+        statusMensagem: "Tarefas deletada com sucesso.",
         resposta: respostaBD,
       });
     } catch (error) {
